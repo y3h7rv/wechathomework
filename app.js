f3f4c5af1c3d1262a92d38df1effd3139801755d
unknown <unknown@example.com>
2023-05-23 10:13:57 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  globalData: {
+      user:{},//后台返回用户全部信息
+    userInfo: {},//微信获取用户信息
+  }
+})
